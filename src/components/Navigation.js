74a354a2b39097a4de3fe1ae67ffaadd8cccdbbe
zlederma <zlederma@ucsd.edu>
2022-09-21 10:29:31 -0500

    Got responsiveness for large breakpoint down

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,14 +5,10 @@ export default function Navigation() {
         <div class="navigation__container">
             <nav>
                 <ul>
-                    <div className="navigation__pages">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Books</a></li>
-                        <li><a href="#">Profile</a></li>
-                    </div>
-                    <div className="navigation__search">
-                        <li><SearchBar /></li>
-                    </div>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Books</a></li>
+                    <li className="navigation__search"><SearchBar /></li>
+                    <li><a href="#">Profile</a></li>
                 </ul>
             </nav>
         </div>
