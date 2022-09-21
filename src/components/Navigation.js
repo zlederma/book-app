af445bc1212e152ea37a8c7f6d48a44dcee43fdf
zlederma <zlederma@ucsd.edu>
2022-09-21 04:19:46 -0500

    Continued Styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,14 @@
 import "./NavigationStyles.css"
 export default function Navigation() {
     return (
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Books</a></li>
-                <li><a href="#">Profile</a></li>
-            </ul>
-        </nav>
+        <div class="nav-container">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Books</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
