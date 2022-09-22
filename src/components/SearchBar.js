f3710729427b0baab502b05f06400ba599dd31c5
zlederma <zlederma@ucsd.edu>
2022-09-22 15:39:33 -0500

    Added placeholder text for the search bar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import "./SearchBarStyles.css"
 export default function SearchBar() {
     return (
         <div className="search-bar__container">
-            <input className="search-bar__search" type="text" />
+            <input className="search-bar__search" type="text" placeholder="Search for a book" />
         </div>
     )
 }
