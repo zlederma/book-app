1e6e37bd218d5899c4c62e22e17cdf71df4714ca
zlederma <zlederma@ucsd.edu>
2022-09-22 11:43:40 -0500

    aligned the input in its container.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,8 @@
 import "./SearchBarStyles.css"
 export default function SearchBar() {
     return (
-        <div class="search-bar__container">
-            <input class="search-bar__search" type="text" />
+        <div className="search-bar__container">
+            <input className="search-bar__search" type="text" />
         </div>
     )
 }
