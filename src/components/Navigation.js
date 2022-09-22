807e18f1047b085cac46f514bfdf147b84e4e65e
zlederma <zlederma@ucsd.edu>
2022-09-22 17:19:20 -0500

    Switched some of the navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,16 @@
 import "./NavigationStyles.css"
 import SearchBar from "./SearchBar"
+import { GiHamburgerMenu } from "react-icons/gi";
 export default function Navigation() {
     return (
-        <div class="navigation__container">
+        <div className="navigation__container">
+            <a ><GiHamburgerMenu /></a>
             <nav>
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Books</a></li>
                     <li className="navigation__search"><SearchBar /></li>
-                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Sign out</a></li>
                 </ul>
             </nav>
         </div>
