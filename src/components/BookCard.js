3bcd42941f9e323ceebbbbc13946b49c73955ece
zlederma <zlederma@ucsd.edu>
2022-09-22 14:06:48 -0500

    Continued styling the book card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,9 +4,11 @@ import AddBookButton from "./AddBookButton";
 export default function BookCard() {
     return (
         <div className="book-card__container">
-            <AddBookButton />
             <div className="book-card__image-container">
                 <img src={data.image} alt="hi" />
+                <div className="book-card__button-container">
+                    <AddBookButton />
+                </div>
             </div>
             <div className="book-card__details-container">
                 <div className="book-card__title">{data.title}</div>
