a4510a669004405bbda4351210f64d3819ac9d90
zlederma <zlederma@ucsd.edu>
2022-09-22 22:29:35 -0500

    Cleaned up button styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -6,9 +6,9 @@ export default function BookCard() {
         <div className="book-card__container">
             <div className="book-card__image-container">
                 <img src={data.image} alt="hi" />
-                <div className="book-card__button-container">
-                    <AddBookButton />
-                </div>
+            </div>
+            <div className="book-card__button-container">
+                <AddBookButton />
             </div>
             <div className="book-card__details-container">
                 <div className="book-card__title">{data.title}</div>
