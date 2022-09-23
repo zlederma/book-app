13983f0031ac07ddd3bbb19530e7cb87ab4250aa
zlederma <zlederma@ucsd.edu>
2022-09-23 15:58:04 -0500

    Started adding styles for the search bar results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,15 @@
 import "./SearchBarStyles.css"
+import { results } from "../mock-book-data"
 export default function SearchBar() {
     return (
-        <div className="search-bar__container">
-            <input className="search-bar__search" type="text" placeholder="Search for a book" />
-        </div>
+        <>
+            <div className="search-bar__container">
+                <input className="search-bar__search" type="text" placeholder="Search for a book" />
+            </div>
+            <div className="search-bar__result-container">
+                <div className="search-bar__result-title">{results[0].title} by</div>
+                <div className="search-bar__result-author">{results[0].author}</div>
+            </div>
+        </>
     )
 }
--- src/mock-book-data.js
+++ src/mock-book-data.js
@@ -3,5 +3,21 @@ export const data = {
     title: "Harry Potter and the Sorcerer...",
     author: "JK Rowling",
     image: image
-
 }
+
+export const results =
+    [
+        {
+            title: "Harry Potter and the Sorcerer's Stone",
+            author: "JK Rowling"
+        },
+        {
+            title: "Kafka on the shore",
+            author: "Haruki Murakami"
+        },
+        {
+            title: "Out of my mind",
+            author: "Sharon Draper"
+        }
+
+    ]
