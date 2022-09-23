10f73a04624f5e2d073566ed04451e745e9214e8
zlederma <zlederma@ucsd.edu>
2022-09-23 10:59:17 -0500

    Added styles for the details container

--- src/mock-book-data.js
+++ src/mock-book-data.js
@@ -1,6 +1,6 @@
 import image from "./assets/mock-book-card-image.jpg"
 export const data = {
-    title: "Harry Potter and the Sorc...",
+    title: "Harry Potter and the Sorcerer...",
     author: "JK Rowling",
     image: image
 
