c56b0ba58684001afc8fa45bb105aa789ebdab60
zlederma <zlederma@ucsd.edu>
2022-09-22 19:41:25 -0500

    Added logic to conditionally render the navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,24 @@
-import "./NavigationStyles.css"
-import SearchBar from "./SearchBar"
+import "./NavigationStyles.css";
+import SearchBar from "./SearchBar";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
 export default function Navigation() {
+    const [navDisplay, setNavDisplay] = useState({});
+    const clickHandler = () => {
+        if (JSON.stringify(navDisplay) === '{}') {
+            setNavDisplay({ display: "block" });
+        }
+        else {
+            setNavDisplay({});
+        }
+    }
     return (
         <div className="navigation__container">
-            <div className="navigation__hamburger-container">
+            <div className="navigation__hamburger-container" onClick={clickHandler}>
                 <a className="navigation__hamburger"><GiHamburgerMenu /></a>
             </div>
-            <nav>
+            <nav className="navigation__navigation" style={navDisplay}>
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Books</a></li>
