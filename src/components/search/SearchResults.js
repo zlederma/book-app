23c457a6989d5d3573d6e1a9688fcbf8bdfcbec6
zlederma <zlederma@ucsd.edu>
2022-09-25 07:01:49 -0500

    Modified styles

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -6,6 +6,8 @@ export default function SearchResults() {
     return (
         <div className="search-results__container">
             <SearchResult />
+            <SearchResult />
+            <SearchResult />
         </div>
     )
 }
