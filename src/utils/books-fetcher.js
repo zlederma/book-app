d6754e7ac455cb3ca299c3422538523ebe233141
zlederma <zlederma@ucsd.edu>
2022-09-25 21:02:26 -0500

    Added a function to clean the data coming in

--- src/utils/books-fetcher.js
+++ src/utils/books-fetcher.js
@@ -1,12 +1,34 @@
 export const getBooks = (query) => {
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&printType=books&orderBy=relevance&maxResults=10&langRestrict=en`).then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not OK');
-        }
-        return response.json()
-    })
-        .then((data) => console.log(data))
-        .catch((error) =>
-            console.error('There has been a problem with your fetch operation:', error)
-        )
+    return (
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&printType=books&orderBy=relevance&maxResults=10&langRestrict=en&projection=lite`).then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not OK');
+            }
+            return response.json()
+        })
+            .then((data) => {
+                return cleanData(data)
+            })
+            .catch((error) =>
+                console.error('There has been a problem with your fetch operation:', error)
+            )
+    )
+}
+
+const cleanData = (data) => {
+    data = data.items;
+    let cleanedData = [];
+    for (let i = 0; i < data.length; i++) {
+        let title = (data[i].volumeInfo.title !== undefined) ? data[i].volumeInfo.title : "";
+        let author = (data[i].volumeInfo.authors[0] !== undefined) ? data[i].volumeInfo.authors[0] : "";
+        //insert a default src
+        let image = (data[i].volumeInfo.imageLinks.thumbnail !== undefined) ? data[i].volumeInfo.imageLinks.thumbnail : "";
+
+        cleanedData.push({
+            title: title,
+            author: author,
+            image: image
+        })
+    }
+    return cleanedData;
 }
