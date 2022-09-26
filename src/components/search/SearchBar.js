e54ddbc36c687b956565d9b278dcc9988328a414
zlederma <zlederma@ucsd.edu>
2022-09-25 22:40:55 -0500

    Started adding logic for when the user adds input

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,16 +1,25 @@
 import "./SearchBarStyles.css"
 import SearchResults from "./SearchResults"
 import { getBooks } from "../../utils/books-fetcher"
+import { useState } from "react"
+
 export default function SearchBar() {
-    getBooks("flowers for alg");
+    const [results, setResults] = useState([]);
+    // getBooks("flowers for alg").then(data => console.log(data));
     const handleChange = (event) => {
-        console.log(event);
-        console.log(event.target.value)
+        const query = event.target.value;
+        if (query.length >= 3) {
+            getBooks(query).then(data => setResults(data));
+        }
     }
+    const handleKeyDown = (event) => {
+    }
+    console.log(results);
+    //On key down===enter is when we want to hide the searchbar and render the cards. 
     return (
         <>
             <div className="search-bar__container">
-                <input className="search-bar__search" type="text" placeholder="Search for a book" onChange={handleChange} />
+                <input className="search-bar__search" type="text" placeholder="Search for a book" onChange={handleChange} onKeyDown={handleKeyDown} />
             </div>
             <SearchResults />
         </>
