9277b49465e9b10e4ef8504c02d6879c2aadecef
zlederma <zlederma@ucsd.edu>
2022-09-25 21:21:49 -0500

    Started setting up handling inputs to the search bar.

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -3,10 +3,14 @@ import SearchResults from "./SearchResults"
 import { getBooks } from "../../utils/books-fetcher"
 export default function SearchBar() {
     getBooks("flowers for alg");
+    const handleChange = (event) => {
+        console.log(event);
+        console.log(event.target.value)
+    }
     return (
         <>
             <div className="search-bar__container">
-                <input className="search-bar__search" type="text" placeholder="Search for a book" />
+                <input className="search-bar__search" type="text" placeholder="Search for a book" onChange={handleChange} />
             </div>
             <SearchResults />
         </>
