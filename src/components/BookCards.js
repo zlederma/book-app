c0a290c0a98feba31b5637aa2b14a6b8439e8bcc
zlederma <zlederma@ucsd.edu>
2022-09-28 12:25:39 -0500

    Error handling

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -5,6 +5,9 @@ import './BookCardsStyles.css'
 export default function BookCards(props) {
     const showBookCards = () => {
         let bookCards = [];
+        if (props.data === undefined) {
+            return <></>;
+        }
         for (let i = 0; i < props.data.length; i++) {
             bookCards.push(<BookCard data={props.data[i]} />)
         }
