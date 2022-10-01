1aa02a2ee5a0c3de1a94fa455a49ab06417d86db
zlederma <zlederma@ucsd.edu>
2022-10-01 17:42:08 -0500

    Added some error handling

--- src/utils/books-fetcher.js
+++ src/utils/books-fetcher.js
@@ -1,3 +1,4 @@
+import defaltSrc from '../assets/default-src.png'
 export const getBooks = (query) => {
     return (
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&printType=books&orderBy=relevance&maxResults=10&langRestrict=en&projection=lite`).then((response) => {
@@ -20,9 +21,9 @@ const cleanData = (data) => {
     let cleanedData = [];
     for (let i = 0; i < data.length; i++) {
         let title = (data[i].volumeInfo.title !== undefined) ? data[i].volumeInfo.title : "";
-        let author = (data[i].volumeInfo.authors[0] !== undefined) ? data[i].volumeInfo.authors[0] : "";
+        let author = (data[i].volumeInfo.authors !== undefined && data[i].volumeInfo.authors.length > 0) ? data[i].volumeInfo.authors[0] : "";
         //insert a default src
-        let image = (data[i].volumeInfo.imageLinks.thumbnail !== undefined) ? data[i].volumeInfo.imageLinks.thumbnail : "";
+        let image = (data[i].volumeInfo.imageLinks.thumbnail !== undefined) ? data[i].volumeInfo.imageLinks.thumbnail : defaltSrc;
 
         cleanedData.push({
             title: title,
