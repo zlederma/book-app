0b8df89ed2cb9d8bce6672a8c6d98dd676bb0498
zlederma <zlederma@ucsd.edu>
2022-10-01 17:48:41 -0500

    Fixed typo

--- src/utils/books-fetcher.js
+++ src/utils/books-fetcher.js
@@ -1,4 +1,4 @@
-import defaltSrc from '../assets/default-src.png'
+import defaultSrc from '../assets/default-src.png'
 export const getBooks = (query) => {
     return (
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&printType=books&orderBy=relevance&maxResults=10&langRestrict=en&projection=lite`).then((response) => {
@@ -23,7 +23,7 @@ const cleanData = (data) => {
         let title = (data[i].volumeInfo.title !== undefined) ? data[i].volumeInfo.title : "";
         let author = (data[i].volumeInfo.authors !== undefined && data[i].volumeInfo.authors.length > 0) ? data[i].volumeInfo.authors[0] : "";
         //insert a default src
-        let image = (data[i].volumeInfo.imageLinks.thumbnail !== undefined) ? data[i].volumeInfo.imageLinks.thumbnail : defaltSrc;
+        let image = (data[i].volumeInfo.imageLinks.thumbnail !== undefined) ? data[i].volumeInfo.imageLinks.thumbnail : defaultSrc;
 
         cleanedData.push({
             title: title,
