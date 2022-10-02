7648d86b6d4b10fd6f3bfe3bf2551457be567cf9
zlederma <zlederma@ucsd.edu>
2022-10-01 21:56:52 -0500

    added a slice for controlling when the results are open or closed

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import bookResultsReducer from '../utils/bookResultsSlice'
+import resultsStateReducer from '../utils/resultsStateSlice'
 
 export default configureStore({
     reducer: {
         bookResults: bookResultsReducer,
+        resultsState: resultsStateReducer,
     },
 })
