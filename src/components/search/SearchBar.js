3fec00fa25bb90a8a1c6041a727f56b1b510b050
zlederma <zlederma@ucsd.edu>
2022-10-01 19:26:08 -0500

    Switched when bookcards would update

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,9 +1,7 @@
-import { createPortal } from "react-dom"
 import "./SearchBarStyles.css"
 import SearchResults from "./SearchResults"
 import { getBooks } from "../../utils/books-fetcher"
 import { useState } from "react"
-import BookCards from '../BookCards'
 import { useSelector, useDispatch } from 'react-redux'
 import { update } from '../../utils/bookResultsSlice'
 
@@ -17,14 +15,14 @@ export default function SearchBar(props) {
         const query = event.target.value;
         if (query.length >= 3) {
             getBooks(query).then(data => setResults(data));
-            dispatch(update(results))
-            console.log(bookResults);
         }
     }
 
     const handleKeyDown = (event) => {
         if (event.key === "Enter") {
             setCloseResults(true);
+            dispatch(update(results))
+            console.log(bookResults);
         }
         else {
             setCloseResults(false);
@@ -39,7 +37,6 @@ export default function SearchBar(props) {
                     onKeyDown={handleKeyDown} />
             </div>
             {!closeResults ? <SearchResults results={results} /> : <></>}
-            {closeResults ? createPortal(<BookCards data={results} />, document.querySelector(".main__container")) : <></>}
         </>
     )
 }
