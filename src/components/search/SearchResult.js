9c596ac854909049b045d31a33cb8a03a2fd7cdd
zlederma <zlederma@ucsd.edu>
2022-10-01 20:38:51 -0500

    Not quite there yet

--- src/components/search/SearchResult.js
+++ src/components/search/SearchResult.js
@@ -1,9 +1,21 @@
 import './SearchResultStyles.css'
+import { getBooks } from "../../utils/books-fetcher"
+import { useState } from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { update } from '../../utils/bookResultsSlice'
+
 
 export default function SearchResult(props) {
+    const dispatch = useDispatch()
     const result = props.result;
+    // const query = `intitle:${result.title}+inauthor:${result.author}`;
+    const query = result.author
+    const handleClick = () => {
+        console.log('clicked')
+        // getBooks(query).then(data => dispatch(update(data)));
+    }
     return (
-        <div className="search-result__container">
+        <div className="search-result__container" onClick={handleClick()}>
             <div className="search-result__title">{result.title}</div>
             <div className="search-result__connective"> by </div>
             <div className="search-result__author">{result.author}</div>
