d866e802e576066a312d1c455ddf5ecb348bd8f8
zlederma <zlederma@ucsd.edu>
2022-10-01 19:25:23 -0500

    Put bookcards in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './MainStyles.css'
+import { useSelector, useDispatch } from 'react-redux'
+import BookCards from './BookCards'
 
 export default function Main() {
+    const bookResults = useSelector((state) => state.bookResults.value)
     return (
         <div className='main__container'>
+            <BookCards data={bookResults} />
         </div>
     )
 }
