1abe138b6772a24ac25d1d2d3bf061919e658843
zlederma <zlederma@ucsd.edu>
2022-10-01 19:26:33 -0500

    Deleted extra code

--- src/utils/bookResultsSlice.js
+++ src/utils/bookResultsSlice.js
@@ -8,16 +8,11 @@ export const bookResultsSlice = createSlice({
     },
     reducers: {
         update: (state, action) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.value = action.payload;
         }
     },
 })
 
-// Action creators are generated for each case reducer function
 export const { update } = bookResultsSlice.actions
 
 export default bookResultsSlice.reducer
