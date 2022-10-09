1ffcf113ea04e382b74a56a6411309752e62500c
zlederma <zlederma@ucsd.edu>
2022-10-09 13:04:36 -0500

    Added logic for the plus and minus button to switch.

--- src/components/BookButton.js
+++ src/components/BookButton.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import "./BookButtonStyles.css"
 import { FiPlus, FiMinus } from "react-icons/fi";
 import { useDispatch } from 'react-redux'
@@ -6,13 +7,24 @@ import { add, remove } from '../utils/librarySlice'
 
 export default function BookButton(props) {
     const dispatch = useDispatch()
-    if (props.button === "minus") {
+    const [button, setButton] = useState(props.button);
+    const plusClickHandler = () => {
+        dispatch(add(props.data));
+        setButton("minus");
+    }
+
+    const minusClickHandler = () => {
+        dispatch(remove(props.data));
+        setButton("plus");
+    }
+
+    if (button === "minus") {
         return (
-            <FiMinus className="book-button__button" strokeWidth="4" stroke="white" strokeLinecap="straight" onClick={() => dispatch(remove(props.data))} />
+            <FiMinus className="book-button__button" strokeWidth="4" stroke="white" strokeLinecap="straight" onClick={minusClickHandler} />
         )
     }
 
     return (
-        <FiPlus className="book-button__button" strokeWidth="4" stroke="white" strokeLinecap="straight" onClick={() => dispatch(add(props.data))} />
+        <FiPlus className="book-button__button" strokeWidth="4" stroke="white" strokeLinecap="straight" onClick={plusClickHandler} />
     )
 }
