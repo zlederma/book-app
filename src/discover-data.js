8adb5a7524386bd480aa000f71189094fae01926
zlederma <zlederma@ucsd.edu>
2022-10-09 05:10:21 -0500

    Added a discover section

---
+++ src/discover-data.js
@@ -0,0 +1,19 @@
+export const discover =
+    [
+        {
+            title: "The World Until Yesterday",
+            author: "Jared Diamond",
+            image: "http://books.google.com/books/content?id=nech105NsugC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        {
+            title: "The Song of Achilles",
+            author: "Madeline Miller",
+            image: "http://books.google.com/books/content?id=opC3d3ltQ94C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        {
+            title: "Algorithms to Live By",
+            author: "Brian Christian",
+            image: "http://books.google.com/books/content?id=Ydn_CwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        }
+
+    ]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import './HomeStyles.css'
 import { useSelector } from 'react-redux'
 import BookCards from '../components/BookCards'
+import { discover } from '../discover-data'
 
 export default function Home() {
     const bookResults = useSelector((state) => state.bookResults.value)
     const library = useSelector((state) => state.library.value)
     return (
         <div className='home__container'>
+            <div className='home__results'>Search Results</div>
             <BookCards data={bookResults} />
+            <div className='home__library'>Library</div>
             <BookCards data={library} button="minus" />
+            <div className='discover__library'>Discover</div>
+            <BookCards data={discover} />
         </div>
     )
 }
