b31555ed8e384058b8addb3debab25811545fe74
zlederma <zlederma@ucsd.edu>
2022-10-09 18:55:38 -0500

    Added logic for showing the different sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,50 @@ import { useSelector } from 'react-redux'
 import BookCards from '../components/BookCards'
 import { discover } from '../discover-data'
 
+const hasBooks = (arr) => {
+    if (arr !== undefined && arr.length > 0) {
+        return true;
+    }
+    return false;
+}
 export default function Home() {
     const bookResults = useSelector((state) => state.bookResults.value)
     const library = useSelector((state) => state.library.value)
+
+    const showSections = () => {
+        const sections = [];
+        if (hasBooks(bookResults)) {
+            sections.push(
+                <>
+                    <div className='home__results'>Search Results</div>
+                    <BookCards data={bookResults} limit="15" />
+                </>
+            )
+        }
+        if (hasBooks(library)) {
+            sections.push(
+                <>
+                    <div className='home__library'>Library</div>
+                    <BookCards data={library} button="minus" />
+                </>
+            )
+        }
+        sections.push(
+            <>
+                <div className='discover__library'>Discover</div>
+                <BookCards data={discover} limit="10" />
+            </>
+        )
+        return sections;
+    }
+
     return (
         <div className='home__container'>
-            <div className='home__results'>Search Results</div>
-            <BookCards data={bookResults} limit="15" />
-            <div className='home__library'>Library</div>
-            <BookCards data={library} button="minus" />
-            <div className='discover__library'>Discover</div>
-            <BookCards data={discover} limit="10" />
+            {showSections()}
         </div>
     )
+
 }
+
+
 
