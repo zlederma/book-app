ee54057406a2f832ce928699e6eec54f34845d04
zlederma <zlederma@ucsd.edu>
2022-10-09 19:53:22 -0500

    Fixed home logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,36 +13,18 @@ export default function Home() {
     const bookResults = useSelector((state) => state.bookResults.value)
     const library = useSelector((state) => state.library.value)
 
-    const showSections = () => {
-        const sections = [];
-        if (hasBooks(bookResults)) {
-            sections.push(
-                <>
-                    <div className='home__results'>Search Results</div>
-                    <BookCards data={bookResults} limit="15" />
-                </>
-            )
-        }
-        if (hasBooks(library)) {
-            sections.push(
-                <>
-                    <div className='home__library'>Library</div>
-                    <BookCards data={library} button="minus" />
-                </>
-            )
-        }
-        sections.push(
-            <>
-                <div className='discover__library'>Discover</div>
-                <BookCards data={discover} limit="10" />
-            </>
-        )
-        return sections;
-    }
-
     return (
         <div className='home__container'>
-            {showSections()}
+            {hasBooks(bookResults) ? <>
+                <div className='home__results'>Search Results</div>
+            </> : <></>}
+            <BookCards data={bookResults} limit="15" />
+            {hasBooks(library) ? <>
+                <div className='home__library'>Library</div>
+            </> : <></>}
+            <BookCards data={library} button="minus" />
+            <div className='discover__library'>Discover</div>
+            <BookCards data={discover} limit="10" />
         </div>
     )
 
